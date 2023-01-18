b80a9e0e614dc984a636890bb83f971437b90e3c
zabelmarta <104087600+zabelmarta@users.noreply.github.com>
2023-01-18 21:36:49 +0100

    Create task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+const textInput = document.querySelector('#name-input');
+
+const output = document.querySelector('#name-output');
+
+textInput.addEventListener('input', event => {
+    output.textContent = event.target.value
+
+    if (event.target.value === '') {
+        output.textContent = 'Anonymous'
+    }
+})
