1fc45d7244b0feab80fba5c0003f9e5958499a24
zabelmarta <104087600+zabelmarta@users.noreply.github.com>
2023-01-19 13:53:07 +0100

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -5,4 +5,4 @@ console.log("Number of categories: ", arrayList.length);
 
 
 
-const categoryElementsCount = liList.forEach((e)=>{console.log(`Category: ${e.querySelector('h2').textContent}\nElements: ${e.querySelector('ul').childElementCount}`)})
+const categoryElementsCount = liList.forEach((e) => { console.log(`Category: ${e.querySelector('h2').textContent}\nElements: ${e.querySelector('ul').childElementCount}`) })
